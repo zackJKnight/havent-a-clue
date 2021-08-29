782792577c39a1ad2d99fe09a2d67dd4c148acbf
zachary knight <zachary.j.knight@gmail.com>
2021-08-29 14:56:34 -0500

    put cards in grids

--- src/PickCards.tsx
+++ src/PickCards.tsx
@@ -1,24 +1,58 @@
-import { Checkbox, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Checkbox, Grid, Paper } from "@material-ui/core";
 import { ClueCard } from "./Model/ClueCard";
+import { makeStyles } from '@material-ui/core/styles';
 
-let cardElements;
+let cardElements: any[];
 
+const useStyles = makeStyles((theme) => ({
+    container: {
+        maxWidth: "lg"
+    },
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 export default function PickCards(props: any) {
-
-    cardElements = props.cards?.map((card: ClueCard, i: number) =>
-        <ListItem key={i}>
-            <ListItemIcon>
+    const classes = useStyles();
+    let tempElements = [];
+    tempElements.push(props.cards?.filter((card: ClueCard) => card.Category === 'suspect')
+    .map((card: ClueCard, i: number) =>
+        <Grid item key={card.Name} spacing={3}>
+            <Paper className={classes.paper}>
                 <Checkbox onChange={(e) => props.onChange(e, card)}></Checkbox>
-            </ListItemIcon>
-            <ListItemText primary={card.Name} >
-            </ListItemText>
-        </ListItem>
-    );
+                {card.Name}
+            </Paper>
+        </Grid>
+    ));
+    tempElements.push(props.cards?.filter((card: ClueCard) => card.Category === 'weapon')
+    .map((card: ClueCard, i: number) =>
+        <Grid item key={card.Name} >
+            <Paper className={classes.paper}>
+                <Checkbox onChange={(e) => props.onChange(e, card)}></Checkbox>
+                {card.Name}
+            </Paper>
+        </Grid>
+    ));
+    tempElements.push(props.cards?.filter((card: ClueCard) => card.Category === 'scene')
+    .map((card: ClueCard, i: number) =>
+        <Grid item key={card.Name}>
+            <Paper className={classes.paper}>
+                <Checkbox onChange={(e) => props.onChange(e, card)}></Checkbox>
+                {card.Name}
+            </Paper>
+        </Grid>
+    ));
+    cardElements = tempElements;
 
     return (
-        <div>
-            <List>{cardElements}</List>
+        <div className={classes.root}>
+            <Grid container spacing={3}>{cardElements}</Grid>
         </div>
     );
 }
