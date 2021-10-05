b7677ba6100c64721addf841ffd128678b2cf18f
zachary knight <zachary.j.knight@gmail.com>
2021-10-04 22:06:47 -0500

    restore bg color

--- src/PickCards.tsx
+++ src/PickCards.tsx
@@ -1,13 +1,13 @@
-import { Card, Checkbox, Grid, GridList, Paper } from "@material-ui/core";
+import { Card, Grid } from "@material-ui/core";
 import { ClueCard } from "./Model/ClueCard";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import ClueCardView from "./ClueCardView";
 
 let suspectElements;
 let weaponElements;
 let sceneElements;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles<Theme>((theme) => ({
     container: {
         display: "flex",
         flexDirection: 'row',
@@ -33,7 +33,9 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+
 export default function PickCards(props: any) {
+
     const classes = useStyles();
 
     function oneSelectionPerCategory() {
@@ -43,20 +45,13 @@ export default function PickCards(props: any) {
     suspectElements = props.cards?.filter((card: ClueCard) => card.Category === 'suspect')
         .map((card: ClueCard) =>
         <Grid item key={card.Name} >
-                <Paper className={classes.paper} key={card.Name} style={{ background: `${''}` }}>
-                    <Checkbox onChange={(e) => props.onChange(e, card)} ></Checkbox>
-                    {card.Name}
-                </Paper>
-                </Grid>
+            <ClueCardView onChange={props.onChange} class={classes.paper} card={card} />
+        </Grid>
         );
     weaponElements = props.cards?.filter((card: ClueCard) => card.Category === 'weapon')
         .map((card: ClueCard) =>
-
             <Grid item key={card.Name} >
-                <Paper className={classes.paper} style={{ background: `${getClueCardBackgroundColor(card)}` }}>
-                    <Checkbox onChange={(e) => props.onChange(e, card)}></Checkbox>
-                    {card.Name}
-                </Paper>
+                <ClueCardView onChange={props.onChange} class={classes.paper} card={card} />
             </Grid>
         );
     sceneElements = props.cards?.filter((card: ClueCard) => card.Category === 'scene')
@@ -83,23 +78,3 @@ export default function PickCards(props: any) {
         </div>
     );
 }
-
-const getClueCardBackgroundColor = (card: ClueCard) => {
-    // TODO color heldby per player
-    // TODO when NotHeldBy array contains all players, color it as a known accusation
-    if (card === undefined) {
-        return;
-    }
-
-    if (!isNaN(card.HeldBy)) {
-        card.BackgroundColor = 'gray';
-    }
-
-    if (card.PossShownBy?.length === 0) {
-        return;
-    }
-    let gradientPercent = ((card.PossShownBy?.length || 0) / .1);
-
-    card.BackgroundColor = `linear-gradient(45deg, #f78da7 ${100 - gradientPercent}%, #b80000 ${gradientPercent}%)`;
-}
-
