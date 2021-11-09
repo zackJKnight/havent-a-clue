f578d855501b88c4e057b141e9af5c96dd5e969b
zachary knight <zachary.j.knight@gmail.com>
2021-11-08 20:18:35 -0600

    space and stuff

--- src/ClueCardView.tsx
+++ src/ClueCardView.tsx
@@ -5,12 +5,10 @@ import { ClueCard } from "./Model/ClueCard";
 export default function ClueCardView(props: any) {
     const [card] = useState<ClueCard>(props.card);
     return(
-        <>
         <Paper className={props.class} style={{ background: `${getClueCardBackgroundColor(props.card)}` }}>
-                    <Checkbox onChange={(e) => props.onChange(e, card)}></Checkbox>
-                    {props.card.Name}
-                </Paper>
-        </>
+            <Checkbox onChange={(e) => props.onChange(e, card)}/>
+            {props.card.Name}
+        </Paper>
     );
 }
 
