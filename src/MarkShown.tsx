580bea2bc289992843fc7b3ec5840e184bcc3f45
zachary knight <zachary.j.knight@gmail.com>
2021-11-22 10:06:37 -0600

    show suggestions while marking shown card

--- src/MarkShown.tsx
+++ src/MarkShown.tsx
@@ -1,136 +1,41 @@
 import { Button, Card, FormControlLabel, Paper, Radio, RadioGroup } from "@material-ui/core";
-import { ChangeEvent, useState } from "react";
-import { useHistory } from "react-router-dom";
 import { ClueCard } from "./Model/ClueCard";
 import { useStyles } from "./Utils/Styles";
 import { Game } from "./Model/Game";
+import { ChangeEvent, useState } from "react";
 
 export default function MarkShown(props: any) {
-    const history = useHistory();
     const classes = useStyles();
 
     // TODO if player is you, show only cards from hand. cant show card if not in hand
     const game: Game = props.game;
-    const [cards, updateCards] = useState<Array<ClueCard>>(props.cards);
-    let heldBy = props.showingPlayerId; //parseInt(props.matchProps.match.params.showingPlayerId.replace(':', ''));
+    const [cards, updateCards] = useState(props.cards);
     //let nextPlayerId = props.nextPlayerId; //parseInt(props.matchProps.match.params.nextPlayerId.replace(':', ''));
-    const [value, setValue] = useState('');
-    const playerId = props.showingPlayerId;
-    const playersAfter = props.game.players.slice(playerId);
-    const playersBefore = props.game.players.slice(0, playerId);
-    const playerTurnOrder = [...playersAfter, ...playersBefore];
-
-    const [nextPlayerId] = useState({ ...playerTurnOrder[1] }.id);
-    const [showingPlayerId, setShowingPlayer] = useState({ ...playerTurnOrder[1] }.id);
-
-    const [answeredNoLink] = useState(`/show:${playerId}`);
+    const [value, setRadioValue] = useState(props.radioValue);
 
     function onOK() {
         if (value === 'None') {
-            onNo();
+            props.onNoneShown();
         } else {
-            onYes();
+            props.onCardShown();
         }
-    }
-
-    function onNo() {
-
-        // add the Showing Player to the NotHeldby array of the isSuggestion cards.
-        const tempCards = [...cards];
-        tempCards.forEach(card => {
-            if (card.isSuggestion) {
-                card.NotHeldBy.push(showingPlayerId);
-            }
-        });
-        updateCards([...tempCards]);
-
-        let showingPlayerIndex = playerTurnOrder.indexOf(playerTurnOrder.filter(player => player.id === showingPlayerId)[0]);
-
-        const nextShowingPlayerIndex = showingPlayerIndex + 1;
-
-        // increment showing player- current showing player did not show a card
-        if (playerTurnOrder[nextShowingPlayerIndex]) {
-            setShowingPlayer(playerTurnOrder[nextShowingPlayerIndex].id);
-        }
-        // if this is the last showing player - noone has shown
-        if (nextShowingPlayerIndex > props.game.players.length - 1) {
-            noteKnownSolutionCards();
-            clearSuggestions();
-            history.push(`/turn:${nextPlayerId}`);
-        } else {
-            history.push(answeredNoLink);
-        }
-
-    }
-
-    function onYes() {
-        // if no cards in the suggestion are known, mark them all as 'possibly shown by'
-        // and move to next turn
-
-        const tempCards = [...cards];
-        const suggestions: Array<ClueCard> = tempCards.filter(card => card.isSuggestion);
-        for (let card of suggestions) {
-            if (!isNaN(card.HeldBy)) {
-                const cardIndex = suggestions.indexOf(card);
-                const knownCard = suggestions.splice(cardIndex, 1);
-                tempCards.forEach((card: ClueCard) => {
-                    if (card.Name === knownCard[0].Name) {
-                        card.isSuggestion = false;
-                    }
-                })
-            }
-        }
-        if (suggestions.length === 1) {
-            // if only one card is not known as held, that card is held by the showing player
-            tempCards.filter(card => card.Name === suggestions[0].Name)[0].HeldBy = showingPlayerId;
-
-        } else {
-            if (suggestions.length > 1) {
-                tempCards.filter(card => suggestions.includes(card)).forEach(tempCard => {
-                    tempCard.PossShownBy.push(parseInt(showingPlayerId));
-                });
-            }
-        }
-        updateCards([...tempCards]);
-
-        clearSuggestions();
-
-        history.push(`/turn:${nextPlayerId}`);
-    }
-
-    function noteKnownSolutionCards() {
-        // if the NOT_HELD_BY list for a card contains all players,
-        // mark as a known accusation.
-        let updatedCards = [...cards];
-        updatedCards.forEach((item: ClueCard) => {
-            if (props.game.players.length === item.NotHeldBy.length) {
-                item.isSolution = true;
-            }
-        });
-        updateCards(updatedCards);
-    }
-
-    function clearSuggestions() {
-        let updatedCards = [...cards];
-        updatedCards.forEach((item: ClueCard) => item.isSuggestion = false);
-        updateCards(updatedCards);
     }
 
     function toggleCardSelection(event: ChangeEvent<HTMLInputElement>) {
-if(event === undefined){
-    return;
-}
-        setValue(event?.target?.value);
+        if (event === undefined) {
+            return;
+        }
+        setRadioValue(event?.target?.value);
         // now that markshown has moved into show, the onNo logic goes here when 
         // the 'none' radio is selected.
 
         if (!event.target.checked) {
-            heldBy = NaN;
+            props.setShowingPlayer(NaN);
         }
 
         let updatedCards = [...cards]
         updatedCards.filter((item: ClueCard) => item.Name === event.target.value)
-            .forEach((item: ClueCard) => item.HeldBy = heldBy);
+            .forEach((item: ClueCard) => item.HeldBy = props.showingPlayerId);
         updateCards(updatedCards);
     }
     return (
--- src/Show.tsx
+++ src/Show.tsx
@@ -1,10 +1,12 @@
 import { Typography } from "@material-ui/core";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import MarkShown from "./MarkShown";
 import { ClueCard } from "./Model/ClueCard";
 import { Game } from "./Model/Game";
 
 export default function Show(props: any) {
+    const history = useHistory();
 
     const playerId = parseInt(props.matchProps.match.params.playerId.replace(':', ''));
     const playersAfter = props.game.players.slice(playerId);
@@ -12,15 +14,110 @@ export default function Show(props: any) {
     const playerTurnOrder = [...playersAfter, ...playersBefore];
 
     const [nextPlayerId] = useState({ ...playerTurnOrder[1] }.id);
-    const [showingPlayerId] = useState({ ...playerTurnOrder[1] }.id);
+    const [showingPlayerId, setShowingPlayer] = useState({ ...playerTurnOrder[1] }.id);
+    const [radioValue] = useState('None');
 
-    const [cards] = useState<Array<ClueCard>>(props.cards);
+    const [cards, updateCards] = useState<Array<ClueCard>>(props.cards);
     const [game] = useState<Game>(props.game);
+
+    const [answeredNoLink] = useState(`/show:${playerId}`);
+
+
+    function onNoneShown() {
+// BUG possiblyShown isn't cleared when a player doesnt show a card
+        // add the Showing Player to the NotHeldby array of the isSuggestion cards.
+        const tempCards = [...cards];
+        tempCards.forEach(card => {
+            if (card.isSuggestion) {
+                card.NotHeldBy.push(showingPlayerId);
+            }
+        });
+        updateCards([...tempCards]);
+
+        let showingPlayerIndex = playerTurnOrder.indexOf(playerTurnOrder.filter(player => player.id === showingPlayerId)[0]);
+
+        const nextShowingPlayerIndex = showingPlayerIndex + 1;
+
+        // increment showing player- current showing player did not show a card
+        if (playerTurnOrder[nextShowingPlayerIndex]) {
+            setShowingPlayer(playerTurnOrder[nextShowingPlayerIndex].id);
+        }
+        // if this is the last showing player - noone has shown
+        if (nextShowingPlayerIndex > props.game.players.length - 1) {
+            noteKnownSolutionCards();
+            clearSuggestions();
+            history.push(`/turn:${nextPlayerId}`);
+        } else {
+            history.push(answeredNoLink);
+        }
+
+    }
+
+    function onCardShown() {
+        // if no cards in the suggestion are known, mark them all as 'possibly shown by'
+        // and move to next turn
+
+        const tempCards = [...cards];
+        const suggestions: Array<ClueCard> = tempCards.filter(card => card.isSuggestion);
+        for (let card of suggestions) {
+            if (!isNaN(card.HeldBy)) {
+                const cardIndex = suggestions.indexOf(card);
+                const knownCard = suggestions.splice(cardIndex, 1);
+                tempCards.forEach((card: ClueCard) => {
+                    if (card.Name === knownCard[0].Name) {
+                        card.isSuggestion = false;
+                    }
+                })
+            }
+        }
+        if (suggestions.length === 1) {
+            // if only one card is not known as held, that card is held by the showing player
+            tempCards.filter(card => card.Name === suggestions[0].Name)[0].HeldBy = showingPlayerId;
+
+        } else {
+            if (suggestions.length > 1) {
+                tempCards.filter(card => suggestions.includes(card)).forEach(tempCard => {
+                    tempCard.PossShownBy.push(parseInt(showingPlayerId));
+                });
+            }
+        }
+        updateCards([...tempCards]);
+
+        clearSuggestions();
+
+        history.push(`/turn:${nextPlayerId}`);
+    }
+
+    function noteKnownSolutionCards() {
+        // if the NOT_HELD_BY list for a card contains all players,
+        // mark as a known accusation.
+        let updatedCards = [...cards];
+        updatedCards.forEach((item: ClueCard) => {
+            if (props.game.players.length === item.NotHeldBy.length) {
+                item.isSolution = true;
+            }
+        });
+        updateCards(updatedCards);
+    }
+
+    function clearSuggestions() {
+        let updatedCards = [...cards];
+        updatedCards.forEach((item: ClueCard) => item.isSuggestion = false);
+        updateCards(updatedCards);
+    }
 
     return (
         <>
             <Typography variant='h3'>{`Shown by player ${showingPlayerId + 1} to player ${playerId + 1}`}</Typography>
-            <MarkShown game={game} cards={cards} showingPlayerId={showingPlayerId} nextPlayerId={nextPlayerId} />
+            <MarkShown
+                game={game} 
+                cards={cards} 
+                showingPlayerId={showingPlayerId}
+                setShowingPlayer={setShowingPlayer} 
+                nextPlayerId={nextPlayerId}
+                onNoneShown={onNoneShown}
+                onCardShown={onCardShown}
+                radioValue={radioValue} />
         </>
     )
 }
